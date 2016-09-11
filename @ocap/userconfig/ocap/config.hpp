845b34f912efbd665dddd008e2ddf0813ade9766
Jamie <goodsonjamie@yahoo.co.uk>
2016-09-11 17:35:05 +0100

    Edited default userconfig settings.

--- @ocap/userconfig/ocap/config.hpp
+++ @ocap/userconfig/ocap/config.hpp
@@ -27,11 +27,11 @@ ocap_exportPassword = "";
 Configure these values if ocap_exportRemote = false
 */
 // Absolute path to OCAP web root directory (must include trailing '/')
-ocap_exportPath = "F:/xampp/htdocs/"; // e.g. "C:/apache/htdocs/ocap/"
+ocap_exportPath = "C:/apache/htdocs/ocap/"; // e.g. "C:/apache/htdocs/ocap/"
 // == Local details end ==
 
 
-ocap_exportURL = "http://localhost/"; // URL to OCAP root directory (must include trailing '/')
+ocap_exportURL = "http://localhost/ocap/"; // URL to OCAP root directory (must include trailing '/')
 ocap_frameCaptureDelay = 1; // Delay between each frame capture. Default: 1
 ocap_minPlayerCount = 4; // Minimum player count before capture begins. Set this to 0 for immediate capture (assuming ocap_endCaptureOnNoPlayers = false)
 
